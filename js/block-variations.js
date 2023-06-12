f54c46cfb891844f35ee3e5405c75de6a211986a
Steve Ryan <steve.ryan@asu.edu>
2023-06-12 16:01:50 -0700

    Add JS event listenener for date picker change

--- js/block-variations.js
+++
@@ -1,9 +0,0 @@
-/**
- * JS file to add block styles and variations to various core blocks.
- * - core/paragraph, add lead style.
- *
- */
-
-wp.domReady(() => {
-
-});
---
+++ js/fourtyfour-day.js
@@ -0,0 +1,41 @@
+/**
+ * Event listener for the 44 day tracker shortcode.
+ * - core/paragraph, add lead style.
+ *
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	function updateCalcDates() {
+		var startDateInput = document.querySelector('#fourty-tracker input#start');
+		var calcDatesElement = document.querySelector('#fourty-tracker .calc-dates');
+
+		// Check if the element exists before adding the event listener
+		if (startDateInput) {
+			startDateInput.addEventListener('change', function () {
+				var startDate = new Date(startDateInput.value);
+				var future30Date = new Date(startDate.getTime() + (30 * 24 * 60 * 60 * 1000));
+				var future44Date = new Date(startDate.getTime() + (44 * 24 * 60 * 60 * 1000));
+				var future45Date = new Date(startDate.getTime() + (45 * 24 * 60 * 60 * 1000));
+
+				var options = { year: 'numeric', month: 'long', day: 'numeric' };
+				var formatted30Date = future30Date.toLocaleDateString(undefined, options);
+				var formatted44Date = future44Date.toLocaleDateString(undefined, options);
+				var formatted45Date = future45Date.toLocaleDateString(undefined, options);
+
+				var dd30Element = calcDatesElement.querySelector('dd.day-30');
+				var dd44Element = calcDatesElement.querySelector('dd.day-44');
+				var dd45Element = calcDatesElement.querySelector('dd.day-45');
+
+				dd30Element.textContent = formatted30Date;
+				dd44Element.textContent = formatted44Date;
+				dd45Element.textContent = formatted45Date;
+			});
+		} else {
+			console.log('Not listening.');
+		}
+	}
+
+	// Call the function to set up the event listener
+	updateCalcDates();
+});

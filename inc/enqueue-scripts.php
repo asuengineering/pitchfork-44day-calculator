6138ecc7b52a657ecbaaff6a83292e8b4e1c4324
Steve Ryan <steve.ryan@asu.edu>
2023-10-03 10:01:10 -0700

    Only enqueue assets if shortcode is present

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -13,12 +13,19 @@
 add_action( 'enqueue_block_assets', 'starter_plugin_enqueue_block_scripts' );
 function starter_plugin_enqueue_block_scripts() {
 
-		$the_plugin     = get_plugin_data( plugin_dir_path( __DIR__ ) . 'fourtyfour-day.php' );
-		$the_version    = $the_plugin['Version'];
-		$style_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'css/fourtyfour-day.css' );
-		$js_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'js/fourtyfour-day.js' );
+	global $post;
 
-	wp_enqueue_style ( 'pf-fourtyfour-day', plugin_dir_url( __DIR__ ) . 'css/fourtyfour-day.css',  array(), $style_version);
-	wp_enqueue_script( 'pf-fourtyfour-day', plugin_dir_url( __DIR__ ) . 'js/fourtyfour-day.js',  array(), $js_version, true);
+	$the_plugin     = get_plugin_data( plugin_dir_path( __DIR__ ) . 'fourtyfour-day.php' );
+	$the_version    = $the_plugin['Version'];
+	$style_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'css/fourtyfour-day.css' );
+	$js_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'js/fourtyfour-day.js' );
+
+	wp_register_style ( 'pf-fourtyfour-day-style', plugin_dir_url( __DIR__ ) . 'css/fourtyfour-day.css',  array(), $style_version);
+	wp_register_script( 'pf-fourtyfour-day', plugin_dir_url( __DIR__ ) . 'js/fourtyfour-day.js',  array(), $js_version, true);
+
+	if( has_shortcode( $post->post_content, '44day-tracker') && ( is_single() || is_page() ) ){
+		wp_enqueue_style ( 'pf-fourtyfour-day-style' );
+		wp_enqueue_script( 'pf-fourtyfour-day' );
+	}
 
 }
